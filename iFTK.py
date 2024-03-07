dae7a731a0f2f3d49ce5e068df39bc54424415f9
Eric <th4tpali@gmail.com>
2024-03-06 18:11:04 -0600

    Update iFTK.py

--- iFTK.py
+++ iFTK.py
@@ -39,7 +39,7 @@ _d = "#DEDE00" # Red - for errors
 _y = "#9EBB00" # Yellow - for warnings
 
 pyqtcss.get_style("dark_blue")
-app_version = 'v3.3-1229' # App version
+app_version = 'v3.9-0306' # App version
 download_urls = {} # Used to pass URLs to the downloader module  
 no_update = False # Do not update QTreeWidget when checking for database update, if there is no updates available
 dbs = [
